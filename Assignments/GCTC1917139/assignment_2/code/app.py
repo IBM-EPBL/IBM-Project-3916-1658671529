566a477c84f2e1dd65c5dc0f3c277150187e88af
Ranjani <ranjanikarthi01@gmail.com>
2022-11-11 21:33:57 +0530

    final project updated

--- Assignments/GCTC1917139/assignment_2/code/app.py
+++ Assignments/GCTC1917139/assignment_2/code/app.py
@@ -8,7 +8,7 @@ from datetime import datetime
 app = Flask(__name__) #constructor
 app.secret_key='a'
 
-conn=ibm_db.connect("DATABASE=bludb;HOSTNAME=b1bc1829-6f45-4cd4-bef4-10cf081900bf.c1ogj3sd0tgtu0lqde00.databases.appdomain.cloud;PORT=32304;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=cpg44748;PWD=rOHvtMTyU4vp6f2v",'','')
+conn=ibm_db.connect("DATABASE='';HOSTNAME='';PORT='';SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID='';PWD=''",'','')
 
 
 @app.route("/", methods=['GET']) #binding url
